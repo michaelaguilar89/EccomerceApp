6f4db6133c5a98b1fb3d0da9c1017d81b0013afe
MichaelAguilar89 <mchlaguilar051@hotmail.es>
2023-09-27 12:29:39 -0600

    adding components,models, services,boostrap reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { CartComponent } from './components/cart/cart.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    CartComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
